eb5dc34f602c7c69f9d3a3eb2ee2f032603ec1d2
otutay <otutaysalgir@gmail.com>
2021-08-05 16:17:19 +0300

    osmant ek

--- UART_receiver.vhd
+++ UART_receiver.vhd
@@ -1,5 +1,5 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.std_logic_arith.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -11,105 +11,107 @@ use IEEE.std_logic_arith.all;
 --use UNISIM.VComponents.all;
 
 entity uart_rcv is
-    
-    Generic ( clk_frq : integer := 100e6;
-              baud: integer := 115200;
-              stopbit: integer :=2
-              
-              );
-              
-    Port ( start_clk : inout std_logic;
-            clk : in STD_LOGIC;
-           dout:  out std_logic_vector(7 downto 0);
-           rx : in std_logic;
-           rx_done : out std_logic);
+
+  generic (clk_frq : integer := 100e6;
+           baud    : integer := 115200;
+           stopbit : integer := 2
+
+           );
+
+  port (start_clk : inout std_logic;
+        clk       : in    std_logic;
+        dout      : out   std_logic_vector(7 downto 0);
+        rx        : in    std_logic;
+        rx_done   : out   std_logic
+        );
 
 
-    
+
 end uart_rcv;
 
 architecture Behavioral of uart_rcv is
-    constant c_bittimerlim 	: integer := clk_frq/baud;
-    constant c_stopbitlim 	: integer := (clk_frq/baud)*stopbit;
-    constant starttimerlim: integer := clk_frq/(baud*32);  -- ask what should be the freq that controls if the process is started 
-                                                           -- (bittime'dan 32 kat daha küçük bir zaman aralığı start state i gözlemlemek için yeterli olur 
-                                                           -- dedim ama açıkcası bilmiyorum)
-    
-    type states is (idle,start,data,stop);
-    signal state: states := idle;
-       
-    signal bittimer : integer range 0 to c_stopbitlim := 0;
-    signal bitcntr	: integer range 0 to 7 := 0;
-    signal shreg	: std_logic_vector (7 downto 0) := (others => '0');
-    signal r_begin : std_logic := '0';
-    signal starttimer: integer range 0 to starttimerlim:= 0;
+  constant c_bittimerlim : integer := clk_frq/baud;
+  constant c_stopbitlim  : integer := (clk_frq/baud)*stopbit;
+  constant starttimerlim : integer := clk_frq/(baud*32);  -- ask what should be the freq that controls if the process is started
+                                                          -- (bittime'dan 32 kat daha küçük bir zaman aralığı start state i gözlemlemek için yeterli olur
+                                        -- dedim ama açıkcası bilmiyorum)
+
+  type states is (idle, start, data, stop);
+  signal state : states := idle;
+
+  signal bittimer   : integer range 0 to c_stopbitlim  := 0;
+  signal bitcntr    : integer range 0 to 7             := 0;
+  signal shreg      : std_logic_vector (7 downto 0)    := (others => '0');
+  signal r_begin    : std_logic                        := '0';
+  signal starttimer : integer range 0 to starttimerlim := 0;
 begin
- 
-  
-P_MAIN : process (clk) begin
-if (rising_edge(clk)) then
-case state is
-	
-		when idle =>
-		
-			rx_done	<= '0';
-			bitcntr			<= 0;
-			
-			if (starttimer = starttimerlim -1) then
-			     if (rx = '0') then
-				    state	<= start;
-				  
-			    
-			     end if;    
-        	else
-        	   starttimer <= starttimer +1;
-        	end if;	    
-		when start =>
-			if (bittimer = (c_bittimerlim*3 / 2)-1 ) then -- start state'e ilk geçildiği andan 3/2 bittime sonrası ilk bitin(D0) ortasına denk gelir diye düşündüm)
-				state				<=data;
-				dout(0)				<= rx;    
-				bittimer			<= 0;
-			else
-				bittimer			<= bittimer + 1;
-			end if;
-			
-		when data =>
-		
-			
-		
-			if (bitcntr = 7) then
-				if (bittimer = c_bittimerlim-1) then
-					
-					bitcntr				<= 0;
-					state				<= stop;
-					bittimer			<= 0;
-				else
-					bittimer			<= bittimer + 1;					
-				end if;			
-			else
-				if (bittimer = c_bittimerlim-1) then
-					dout(bitcntr)          <= rx;
-					bitcntr				<= bitcntr + 1;
-					bittimer			<= 0;
-				else
-					bittimer			<= bittimer + 1;					
-				end if;
-			end if;
-		
-		when stop =>
-		
-			if (bittimer = c_stopbitlim-1) then
-				state				<= idle;
-				rx_done		<= '1';
-				bittimer			<= 0;
-			else
-				bittimer			<= bittimer + 1;				
-			end if;		
-	
+
+
+  P_MAIN : process (clk)
+  begin
+    if (rising_edge(clk)) then
+      case state is
+
+        when idle =>
+
+          rx_done <= '0';
+          bitcntr <= 0;
+
+          if (starttimer = starttimerlim -1) then
+            if (rx = '0') then
+              state <= start;
+
+
+            end if;
+          else
+            starttimer <= starttimer +1;
+          end if;
+        when start =>
+          if (bittimer = (c_bittimerlim*3 / 2)-1) then  -- start state'e ilk geçildiği andan 3/2 bittime sonrası ilk bitin(D0) ortasına denk gelir diye düşündüm)
+            state    <= data;
+            dout(0)  <= rx;
+            bittimer <= 0;
+          else
+            bittimer <= bittimer + 1;
+          end if;
+
+        when data =>
 
 
 
-end case;
-end if;
-end process;
+          if (bitcntr = 7) then
+            if (bittimer = c_bittimerlim-1) then
+
+              bitcntr  <= 0;
+              state    <= stop;
+              bittimer <= 0;
+            else
+              bittimer <= bittimer + 1;
+            end if;
+          else
+            if (bittimer = c_bittimerlim-1) then
+              dout(bitcntr) <= rx;
+              bitcntr       <= bitcntr + 1;
+              bittimer      <= 0;
+            else
+              bittimer <= bittimer + 1;
+            end if;
+          end if;
+
+        when stop =>
+
+          if (bittimer = c_stopbitlim-1) then
+            state    <= idle;
+            rx_done  <= '1';
+            bittimer <= 0;
+          else
+            bittimer <= bittimer + 1;
+          end if;
+
+
+
+
+      end case;
+    end if;
+  end process;
 end Behavioral;
--- UART_top.vhd
+++ UART_top.vhd
@@ -16,15 +16,15 @@ end UART_top;
 
 architecture Behavioral of UART_top is
 
-  signal txStart  : std_logic              := '0';
-  constant second : integer                := 200000000;
-  signal seccntr  : integer range 0 to second := 0;
-  signal d_in     : std_logic_vector(7 downto 0):="10100101";
+  signal txStart  : std_logic                    := '0';
+  constant second : integer                      := 200000000;
+  signal seccntr  : integer range 0 to second    := 0;
+  signal d_in     : std_logic_vector(7 downto 0) := "10100101";
 
   component uart is
     port (clk      : in  std_logic;
           rst      : in  std_logic;
-          din      : in std_logic_vector(7 downto 0);
+          din      : in  std_logic_vector(7 downto 0);
           tx_start : in  std_logic;
           tx_out   : out std_logic;
           tx_done  : out std_logic
@@ -41,21 +41,26 @@ architecture Behavioral of UART_top is
   end component;
   attribute mark_debug : string;
 
-  signal Dout   : std_logic_vector(7 downto 0); 
-  signal txDone : std_logic := '0';
-  signal rxDone : std_logic := '0';
-  signal txOut  : std_logic := '0';
-  signal rxIn   : std_logic := '0';
+  signal Dout         : std_logic_vector(7 downto 0);
+  signal receivedData : std_logic_vector(7 downto 0);
+  signal dataSend     : std_logic_vector(7 downto 0) := "10100101";
+  signal txDone       : std_logic                    := '0';
+  signal rxDone       : std_logic                    := '0';
+  signal txOut        : std_logic                    := '0';
+  signal rxIn         : std_logic                    := '0';
 
-  attribute mark_debug of txOut  : signal is "true";
-  attribute mark_debug of rxIn   : signal is "true";
-  attribute mark_debug of txDone : signal is "true";
-  attribute mark_debug of rxDone : signal is "true";
-  attribute mark_debug of Dout   : signal is "true";
+  attribute mark_debug of txOut        : signal is "true";
+  attribute mark_debug of rxIn         : signal is "true";
+  attribute mark_debug of txDone       : signal is "true";
+  attribute mark_debug of rxDone       : signal is "true";
+  attribute mark_debug of Dout         : signal is "true";
+  attribute mark_debug of receivedData : signal is "true";
+  attribute mark_debug of dataSend     : signal is "true";
+
 begin
   oTxOut <= txOut;
   rxIn   <= iRxIn;
-  dout  <= Dout;
+  dout   <= Dout;
   uartt : process(clock)
   begin
     if (rising_edge(clock)) then
@@ -67,25 +72,47 @@ begin
     end if;
   end process;
 
-  TX : uart
+
+  uart_rcv_1 : entity work.uart_rcv
+    port map (
+      start_clk => '0',
+      clk       => clock,
+      dout      => receivedData,
+      rx        => rxIn,
+      rx_done   => rxDone
+      );
+
+  uart_1 : entity work.uart
     port map (
       clk      => clock,
       rst      => reset,
+      din      => dataSend,
       tx_start => txStart,
       tx_out   => txOut,
       tx_done  => txDone
       );
 
 
-  RX : uart_rcv
-    port map (
-      clk     => clock,
-      dout    => d_out,
-      rx      => rxIn,
-      rx_done => rxDone
-      );
 
 
-                                                     
+  -- TX : uart
+  --   port map (
+  --     clk      => clock,
+  --     rst      => reset,
+  --     tx_start => txStart,
+  --     tx_out   => txOut,
+  --     tx_done  => txDone
+  --     );
+
+
+  -- RX : uart_rcv
+  --   port map (
+  --     clk     => clock,
+  --     dout    => d_out,
+  --     rx      => rxIn,
+  --     rx_done => rxDone
+  --     );
+
+
+
 end Behavioral;
-
