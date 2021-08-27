a2bf27e7be66e924fdd7881ab2c70b159dfd86dd
Ozgr29Alb <70288280+Ozgr29Alb@users.noreply.github.com>
2021-08-27 13:55:58 +0300

    Update UART_top.vhd

--- UART_top.vhd
+++ UART_top.vhd
@@ -21,7 +21,7 @@ architecture Behavioral of UART_top is
  
 
   signal txStart  : std_logic                    := '0';
-  constant second : integer                      := 10; --200000000;
+  constant second : integer                      := 50000000;
   signal seccntr  : integer range 0 to second    := 0;
   signal d_in     : std_logic_vector(7 downto 0) := "10000001";
 
@@ -60,19 +60,27 @@ end component;
       clk     : in  std_logic;
       dout    : out std_logic_vector(7 downto 0);
       rx      : in  std_logic;
-      rx_done : out std_logic
+      rx_done : out std_logic;
+      valid   : out std_logic
       );
   end component;
+  -------------------------METASTABILITY ISSUES
+  signal rxIn1         : std_logic                    := '0';
+  signal rxIn2         : std_logic                    := '0';
+  signal rxIn3         : std_logic                    := '0';
+  signal rxIn4         : std_logic                    := '0';
+ ------------------------------------------------------------
+ 
   attribute mark_debug : string;
 
-  --signal Dout         : std_logic_vector(7 downto 0);
+  --SIGNALS---------------------------------------------------------
   signal receivedData : std_logic_vector(7 downto 0);
   signal dataSend     : std_logic_vector(7 downto 0) := "10000001";
   signal txDone       : std_logic                    := '0';
   signal rxDone       : std_logic                    := '0';
   signal txOut        : std_logic                    := '0';
   signal rxIn         : std_logic                    := '0';
-
+  signal o_valid      : std_logic                    := '0';
 
   attribute mark_debug of txOut        : signal is "true";
   attribute mark_debug of rxIn         : signal is "true";
@@ -81,57 +89,68 @@ end component;
   --attribute mark_debug of Dout         : signal is "true";
   attribute mark_debug of receivedData : signal is "true";
   attribute mark_debug of dataSend     : signal is "true";
-attribute mark_debug of clk_30        : signal is "true"; 
-attribute mark_debug of clk_60        : signal is "true"; 
-
-
+attribute mark_debug of clk_30         : signal is "true"; 
+attribute mark_debug of clk_60         : signal is "true"; 
+  attribute mark_debug of o_valid      : signal is "true";
+--  
+  attribute mark_debug of rxIn4        : signal is "true";
+--
   attribute KEEP : string;
   --attribute KEEP of Dout: signal is "True";
   attribute KEEP of receivedData: signal is "True";
   attribute KEEP of dataSend: signal is "True";
 begin
   oTxOut     <= txOut;
-  rxIn       <= iRxIn;
-  --dout   <= Dout;
-  --d_out  <= Dout;
-  --d_out <= "00000000";
-  --forDebug <= receivedData OR dataSend OR Dout;
+  rxIn       <= iRxIn;       -------------------------------------????????????????????????????????????
 
-wizard : clk_wiz_0
-   port map ( 
-  -- Clock out ports  
-   clk_30d => clk_30,
-   clk_60d => clk_60,
-   clk_high => clkHigh,
-   -- Clock in ports
-   clk_in1 => clock
- );
+--  synchronizer: process(clk_60)                   -----------------????????????????????????????????
+--    begin
+--    
+--    rxIn       <= iRxIn;
+--    rxIn1      <= rxIn;
+--    rxIn2      <= rxIn1;
+--    rxIn3      <= rxIn2;
+--    rxIn4      <= rxIn3;
+--  end process;
+    
   
---new_data: process(clk_30)
---begin
---if (rising_edge(clk_30)) then
---    if (txDone = '1') then
---        dataSend <= dataSend(dataSend'high-3 downto 0) & dataSend(dataSend'high downto 5);
---    end if;
---end if;
---end process;
+  wizard : clk_wiz_0
+     port map ( 
+    -- Clock out ports  
+     clk_30d => clk_30,
+     clk_60d => clk_60,
+     clk_high => clkHigh,
+     -- Clock in ports
+     clk_in1 => clock
+   );
   
-new_datas: process(clk_30)
-begin
-if (rising_edge(clk_30)) then
-    if (txDone = '1') then
-        if (dataSend <= "10000001") then
-            dataSend <= "10100101";
-       
-        elsif (dataSend <= "10100101") then
-            dataSend <= "10000001";
-        end if;
-    end if;
-end if;
-end process;
+  
+    
+  --new_data: process(clk_30)
+  --begin
+  --if (rising_edge(clk_30)) then
+  --    if (txDone = '1') then
+  --        dataSend <= dataSend(dataSend'high-3 downto 0) & dataSend(dataSend'high downto 5);
+  --    end if;
+  --end if;
+  --end process;
+    
+  new_datas: process(clk_30)
+  begin
+  if (rising_edge(clk_30)) then
+      if (txDone = '1') then
+          if (dataSend <= "10000001") then
+              dataSend <= "10110101";
+         
+          elsif (dataSend <= "10110101") then
+              dataSend <= "10000001";
+          end if;
+      end if;
+  end if;
+  end process;
     
     
-  uartt : process(clk_30)
+  starter : process(clk_30)
   begin
     if (rising_edge(clk_30)) then
       if (seccntr = second - 1) then
@@ -146,7 +165,7 @@ end process;
   uart_1 : entity work.uart
     port map (
       clk      => clk_30,
-      --rst      => reset,
+
       din      => dataSend,
       tx_start => txStart,
       tx_out   => txOut,
@@ -155,36 +174,14 @@ end process;
 
   uart_rcv_1 : entity work.uart_rcv
     port map (
-      --start_clk => '0',
+
       clk       => clk_60,
       dout      => receivedData,
-      rx        => rxIn,
-      rx_done   => rxDone
+      rx        => rxIn,--4,
+      rx_done   => rxDone,
+      valid     => o_valid
       );
 
 
-
-
-
-
-  -- TX : uart
-  --   port map (
-  --     clk      => clock,
-  --     rst      => reset,
-  --     tx_start => txStart,
-  --     tx_out   => txOut,
-  --     tx_done  => txDone
-  --     );
-
-
-  -- RX : uart_rcv
-  --   port map (
-  --     clk     => clock,
-  --     dout    => d_out,
-  --     rx      => rxIn,
-  --     rx_done => rxDone
-  --     );
-
-
-
 end Behavioral;
+
